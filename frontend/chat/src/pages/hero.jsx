43eec2415a03165c3e9c2034ae1446a20aed08f5
chandru262005 <tvchandru12@gmail.com>
2024-09-09 19:25:29 +0530

    Modified timeline

--- frontend/chat/src/pages/hero.jsx
+++ frontend/chat/src/pages/hero.jsx
@@ -15,11 +15,11 @@ const HeroesGallery = () => {
       <div className="gallery">
         {[
          { id: 1, name: 'Kattabomman', path: '/south', image: '/pandiyan.png' },
-          { id: 2, name: 'Puli thevar', path: '/south', image: '/puli thevar.jpg' },
-          { id: 3, name: 'Maruthu brothers', path: '/south', image: '/maruthu brothers.jpg' },
-          { id: 4, name: 'Dheeran chinnamalai', path: '/south', image: '/dheeran.jpg' },
-          { id: 5, name: 'Velunachiyar', path: '/south', image: '/velu.jpg' },
-          { id: 6, name: 'Hero 6', path: '/hero6', image: 'pandiyan.png' }
+          { id: 2, name: 'Dheeran Chinnamalai', path: '/south', image: '/dheeran.jpg' },
+          { id: 3, name: 'Puli Thevar', path: '/south', image: '/puli thevar.jpg' },
+          { id: 4, name: 'Velunachiyar', path: '/south', image: '/velu.jpg' },
+          { id: 5, name: 'Maruthu brothers', path: '/south', image: '/maruthu brothers.jpg'  },
+          { id: 6, name: 'Hyder Ali', path: '/south', image: '/hyder ali.jpeg' }
         ].map(hero => (
           <div className="hero" key={hero.id} onClick={() => navigate(hero.path, { state: { heroName: hero.name } })}>
             <img src={hero.image} alt={hero.name} />
